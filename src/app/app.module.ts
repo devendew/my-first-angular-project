29653d0f73e10cfc62fc2bd63894c9e533f061a0
Devendra Dewangan <devendra.dewangan111111@gmail.com>
2021-07-06 11:25:38 +0530

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CustomersComponent } from './customers/customers.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CustomersComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
